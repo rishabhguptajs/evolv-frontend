322181233c128d515f4a7f1b51c0ad436897013f
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-11-24 12:34:37 +0530

    some changes

---
+++ src/components/Backgrounds/CurvedCircle.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface CurvedDottedBorderProps {
+  color: string;
+}
+
+const CurvedDottedBorder: React.FC<CurvedDottedBorderProps> = ({ color }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CurvedDottedBorder
