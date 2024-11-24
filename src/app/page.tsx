49dfc5e7549d255d167e0c87015abdd494e89b16
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-11-24 12:34:31 +0530

    refactor: Some Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import Navbar from "@/components/Common/Navbar"
-import Image from "next/image"
+"use client";
+
+import Navbar from "@/components/Common/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center m-2">
+      <div className="flex justify-between w-full">
+        <div className="text-5xl text-black cursor-pointer hover:translate-x-1 hover:translate-y-1">Evolv</div>
         <Navbar />
+      </div>
+
+      <div>
+        <div className="flex flex-col items-center py-10">
+            <p className="text-6xl font-medium text-center text-gray-800 mb-4">
+              Think. Grow.
+            </p>
+            <span className="text-6xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-600 bg-opacity-70">
+              Evolve.
+            </span>
+        </div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
